9ed8d84c67647d1c6d7b4dcd2e748a83631bf432
Mathias O'dia Angole <34149930+mat592@users.noreply.github.com>
2022-07-23 22:36:39 +0300

    Components Fix

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeroBanner = () => {
+  return <div>Hero Banner</div>;
+};
+
+export default HeroBanner;
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@ const index = () => {
   return (
     <>
       HeroBanner
-      <div>
+      <div className="products-heading">
         <h2>Best Selling Product</h2>
         <p>Speakers of many Variations</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
     </>
   );
 };
